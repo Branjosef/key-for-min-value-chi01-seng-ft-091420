d09662fc79d8c516f5cdecc61ba14a5dfec9b233
Brandon Fonnemann <branjosef@gmail.com>
2020-08-25 21:05:27 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,12 +2,12 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-
+if hash.class != NilClass
   while name_hash.length != 1 
   test = name_hash.max_by{|k,v| v}
   name_hash.delete(test[0])
   end
   r = name_hash.to_a 
   r[0][0]
-
+end
 end
