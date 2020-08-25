90435963f4c4c5d9cbb3c864a3d0b245ea6c21cd
Brandon Fonnemann <branjosef@gmail.com>
2020-08-25 20:49:22 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -3,9 +3,11 @@
 
 def key_for_min_value(name_hash)
 
-while name_hash.length != 1 
-test = name_hash.max_by{|k,v| v}
-name_hash.delete(test[0])
+if name_hash.class != NilClass
+  while name_hash.length != 1 
+  test = name_hash.max_by{|k,v| v}
+  name_hash.delete(test[0])
+  end
 end
 name_hash
 end
